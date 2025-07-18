e3626984c3754b82618cd2fdf70bb68326d3bf3d
JhonpaulEspiritu13 <jhonpaul.espiritu13@gmail.com>
2025-07-18 10:12:12 -0500

    Fixed bug where Django vibe checker app was not recognized as a module.

--- backend/backend/vibechecker/apps.py
+++ backend/backend/vibechecker/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class VibecheckerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'vibechecker'
+    name = 'backend.vibechecker'
