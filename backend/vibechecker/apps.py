be3994b9f9b7dfd0a71aa42017761d50c32503bc
JhonpaulEspiritu13 <jhonpaul.espiritu13@gmail.com>
2025-07-20 09:09:31 -0500

    Added .gitignore to ignore database files. Placed vibechecker app outside of django project folder, one folder back.

--- backend/vibechecker/apps.py
+++ backend/vibechecker/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class VibecheckerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'backend.vibechecker'
+    name = 'vibechecker'
