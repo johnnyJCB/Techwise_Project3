58b616c092949417330138a28a5a09ce47a1fa0b
JhonpaulEspiritu13 <jhonpaul.espiritu13@gmail.com>
2025-07-20 10:34:03 -0500

    Created model from Sentiment140 dataset. Set-up for Serializer class creation.

--- backend/vibechecker/models.py
+++ backend/vibechecker/models.py
@@ -1,2 +1,46 @@
 # Imports
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+# Classes
+class Sentiment140Item(models.Model):
+    """
+        Kaggle Dataset "Sentiment140 dataset with 1.6 million tweets": https://www.kaggle.com/datasets/kazanova/sentiment140?resource=download
+        
+        Members named and ordered according to the original dataset, with the exception of id (it conflicts with Django field creation). 
+        db_comment field is from db's Kaggle page, which is currently commented out due to SQLite restrictions.
+    """
+    # Enum Classes
+    class Target(models.IntegerChoices):
+        """Integer choice enum class for Sentiment140Item's 'target' member."""
+        NEGATIVE = 0, _("negative")
+        NEUTRAL = 2, _("neutral")
+        POSITIVE = 4, _("positive")
+
+    # Members
+    target = models.IntegerField(
+        choices=Target,
+        #db_comment="the polarity of the tweet (0 = negative, 2 = neutral, 4 = positive)"
+    )
+    tweet_id = models.IntegerField(
+        unique=True,
+        #db_comment="The id of the tweet ( 2087)"
+    )
+    date = models.DateTimeField(
+        #db_comment="the date of the tweet (Sat May 16 23:58:44 UTC 2009)"
+    )
+    flag = models.CharField(
+        max_length=10,
+        #db_comment="The query (lyx). If there is no query, then this value is NO_QUERY."
+    )
+    user = models.CharField(
+        max_length=15,
+        #db_comment="the user that tweeted (robotickilldozr)"
+    )
+    text = models.TextField(
+        max_length=280,
+        #db_comment="the text of the tweet (Lyx is cool). Important Note: Max Length according to normal users, not including Twitter Blue users."
+    )
+
+    def __str__(self):
+        return f'User "{self.user}" said "{self.text}" on {self.date}."'
