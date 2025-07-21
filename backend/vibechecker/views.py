c03eb9a6769937ac673827365c9050d2eb534319
JhonpaulEspiritu13 <jhonpaul.espiritu13@gmail.com>
2025-07-21 13:19:37 -0500

    Basic API is now set up. Django REST Framework using Views has been made. GET, POST, PUT, and DELETE handling returns JSONResponses. CSRF had to be exempted for testing.

--- backend/vibechecker/views.py
+++ backend/vibechecker/views.py
@@ -1,25 +1,70 @@
-# Initially from: https://www.django-rest-framework.org/tutorial/quickstart/
-# Will rework to fit our needs later on.
-
 # Imports
-from django.contrib.auth.models import Group, User
-from rest_framework import permissions, viewsets
-
-from vibechecker.serializers import GroupSerializer, UserSerializer
+# -Django and REST Framework Imports-
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from vibechecker.models import Sentiment140Item
+from vibechecker.serializers import Sentiment140ItemSerializer
 
 # Views
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+@csrf_exempt
+def sentiment_140_item_list(request):
+    """Handle requests that have no slugs. Like POST requests.
+    Important Note: This is currently CSRF Exempt for testing purposes! Be wary.
+    
+    :param request: The request to interact with."""
+    # Gets all items in the Dataset and returns a JSON serialized file back.
+    # May be a bit much, considering we have 1,600,000 items. So be careful.
+    if request.method == "GET":
+        items = Sentiment140Item.objects.all()
+        serializer = Sentiment140ItemSerializer(items, many=True)
 
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all().order_by('name')
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
+        # Return GET info.
+        return JsonResponse(serializer.data, safe=False)
+
+    # Does a new POST request with the given body. Inserts this into the database.
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = Sentiment140ItemSerializer(data=data)
+
+        # Checks if serializer comes back valid. If so, save it to the database.
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def sentiment_140_item_detail(request, id):
+    """Handle requests that interact with only one item in the Database. Requires an ID.
+    Important Note: This is currently CSRF Exempt for testing purposes! Be wary.
+    
+    :param request: The request to interact with.
+    :param id: Int slug that indicates the id field in database."""
+    # Obtains a single item from the dataset, otherwise returns a 404.
+    try:
+        item = Sentiment140Item.objects.get(id=id)
+    except Sentiment140Item.DoesNotExist:
+        return HttpResponse(status=404)
+
+    # Handles GET requests. Returns a serialized item.
+    if request.method == "GET":
+        serializer = Sentiment140ItemSerializer(item)
+        return JsonResponse(serializer.data)
+
+    # Handles PUT requests. If valid, it returns the new data as confirmation. Otherwise it returns the error logs.
+    elif request.method == "PUT":
+        data = JSONParser().parse(request)
+        serializer = Sentiment140ItemSerializer(item, data=data)
+
+        # Checks if serializer comes back valid. If so, save it to the database.
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=404)
+
+    # Handles DELETE requests. Deletes the item from the database.
+    elif request.method == "DELETE":
+        item.delete()
+        return HttpResponse(status=204)
+
+    
