db69cc9e01a5d201b78cfe86ac08eaa7ecc5706a
JhonpaulEspiritu13 <jhonpaul.espiritu13@gmail.com>
2025-07-23 11:31:05 -0500

    Changed Django REST framework function views as Class views.

--- backend/vibechecker/urls.py
+++ backend/vibechecker/urls.py
@@ -5,8 +5,8 @@ from vibechecker import views
 
 # URL Patterns
 urlpatterns = [
-    path("api/v1.0/sentiment_140/", views.sentiment_140_item_list),
-    path("api/v1.0/sentiment_140/<int:id>/", views.sentiment_140_item_detail),
+    path("api/v1.0/sentiment_140/", views.Sentiment140ItemList.as_view()),
+    path("api/v1.0/sentiment_140/<int:id>/", views.Sentiment104DetailList.as_view()),
 ]
 
 # Allows the user to change format (.json) within views.py
--- backend/vibechecker/views.py
+++ backend/vibechecker/views.py
@@ -1,28 +1,26 @@
 # Imports
 # -Django and REST Framework Imports-
-from rest_framework import status
-from rest_framework.decorators import api_view
+from django.http import Http404
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 # -App Imports-
 from vibechecker.models import Sentiment140Item
 from vibechecker.serializers import Sentiment140ItemSerializer
 
-# Views
-@api_view(['GET', 'POST'])
-def sentiment_140_item_list(request, format=None):
-    """List all items in the Sentiment 140 dataset, or create one."""
-    # Gets all items in the Dataset and returns a JSON serialized file back.
-    # May be a bit much, considering we have 1,600,000 items. So be careful.
-    if request.method == "GET":
+class Sentiment140ItemList(APIView):
+    """Returns all items in the Sentiment 140 database, or creates and saves a new entry."""
+    def get(self, request, format=None) -> Response:
+        """GET all items from the database."""
         items = Sentiment140Item.objects.all()
         serializer = Sentiment140ItemSerializer(items, many=True)
 
         # Return GET info.
         return Response(serializer.data)
 
-    # Does a new POST request with the given body. Inserts this into the database.
-    elif request.method == "POST":
+    def post(self, request, format=None) -> Response:
+        """POST a new item to the database."""
         serializer = Sentiment140ItemSerializer(data=request.data)
 
         # Checks if serializer comes back valid. If so, save it to the database.
@@ -31,25 +29,29 @@ def sentiment_140_item_list(request, format=None):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(["GET", "PUT", "DELETE"])
-def sentiment_140_item_detail(request, id, format=None):
-    """Handle requests that interact with only one item in the Database. Requires an ID.
-    
-    :param request: The request to interact with.
-    :param id: Int slug that indicates the id field in database."""
-    # Obtains a single item from the dataset, otherwise returns a 404.
-    try:
-        item = Sentiment140Item.objects.get(id=id)
-    except Sentiment140Item.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class Sentiment104DetailList(APIView):
+    """Returns an individual item from the Sentiment 140 database; updates the item; or deletes the entry entirely."""
+    def get_object(self, id) -> Sentiment140Item:
+        """Obtains a single item from the dataset, otherwise raises an Http404 error."""
+        try:
+            return Sentiment140Item.objects.get(id=id)
+        # If it does not exist in database, raise a specific error.
+        except Sentiment140Item.DoesNotExist:
+            raise Http404
 
-    # Handles GET requests. Returns a serialized item.
-    if request.method == "GET":
+    def get(self, request, id, format=None) -> Response:
+        """GET an item from the database with a given ID."""
+        item = self.get_object(id)
         serializer = Sentiment140ItemSerializer(item)
         return Response(serializer.data)
 
-    # Handles PUT requests. If valid, it returns the new data as confirmation. Otherwise it returns the error logs.
-    elif request.method == "PUT":
+    def put(self, request, id, format=None) -> Response:
+        """
+            PUT and update an item into the database with given body and ID.
+            If valid, it returns the new data as confirmation.
+            Otherwise it returns the error logs.
+        """
+        item = self.get_object(id)
         serializer = Sentiment140ItemSerializer(item, data=request.data)
 
         # Checks if serializer comes back valid. If so, save it to the database.
@@ -58,9 +60,10 @@ def sentiment_140_item_detail(request, id, format=None):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    # Handles DELETE requests. Deletes the item from the database.
-    elif request.method == "DELETE":
+    def delete(self, request, id, format=None) -> Response:
+        """DELETE an item from the database with a given ID."""
+        item = self.get_object(id)
         item.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
     
